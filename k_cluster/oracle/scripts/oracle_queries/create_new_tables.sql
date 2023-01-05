9372b6cec4dde8d3b86c66f8dffe80b2fd969564
Fasih Uddin <fasihuddin1994@gmail.com>
2023-01-05 13:04:08 +0500

    Updated table query

--- k_cluster/oracle/scripts/oracle_queries/create_new_tables.sql
+++ k_cluster/oracle/scripts/oracle_queries/create_new_tables.sql
@@ -9,7 +9,8 @@ CREATE TABLE orders(
     transaction_id NUMBER PRIMARY KEY,
     qty NUMBER(8,0),
     sku_name VARCHAR(100),
-    order_value NUMBER(8,2) -- 20% less than purchase value
+    order_value NUMBER(8,2), -- 20% less than purchase value
+    ordered_at TIMESTAMP
     );
 
 -- create table 2
@@ -18,5 +19,6 @@ CREATE TABLE purchase(
     username VARCHAR(50),
     currency VARCHAR(12),
     purchase_value NUMBER(8,2),
-    country VARCHAR(50)
+    country VARCHAR(50),
+    PURCHASE_TIME TIMESTAMP
     );
